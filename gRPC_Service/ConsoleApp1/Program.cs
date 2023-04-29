d49da37483d38b5b3ce1b988d69efa50e215dad5
timka <mart@develly.ee>
2023-04-29 16:58:14 +0300

    log trains to db

--- gRPC_Service/ConsoleApp1/Program.cs
+++ gRPC_Service/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp1;
 using gRPC_Service;
 using Grpc.Net.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 Console.WriteLine("Hello, World!");
 
@@ -32,8 +35,40 @@ Console.WriteLine("reply: " + reply.Message);
 //
 // ListenServer();
 
+static bool IsValidJson(string strInput)
+{
+    if (string.IsNullOrWhiteSpace(strInput)) { return false;}
+    strInput = strInput.Trim();
+    if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+        (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+    {
+        try
+        {
+            var obj = JToken.Parse(strInput);
+            return true;
+        }
+        catch (JsonReaderException jex)
+        {
+            //Exception in parsing json
+            Console.WriteLine(jex.Message);
+            return false;
+        }
+        catch (Exception ex) //some other exception
+        {
+            Console.WriteLine(ex.ToString());
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
 
 async void ListenServer() {
+    var servElron = new ServElron("Host=localhost;Username=postgres;Password=123;Database=rongisoit");
+
+    
     using (var call = client.SubscribeForDataCollector(new SubscribeRequest { Id = "test1" }))
     {
         var cancellationToken = new CancellationToken();
@@ -43,6 +78,51 @@ async void ListenServer() {
             var update = responseStream.Current;
             // Do something with the update from the server
             Console.WriteLine($"Update received: {update}");
+            
+            var isValidJson = IsValidJson(update.Json);
+            if (isValidJson)
+            {
+                // update train locations
+                var jsonArray = JArray.Parse(update.Json);
+                foreach (JObject jsonObject in jsonArray)
+                {
+                    var reis = (string)jsonObject["reis"];
+                    var liin = (string)jsonObject["liin"];
+                    var reisi_algus_aeg = (string)jsonObject["reisi_algus_aeg"];
+                    var reisi_lopp_aeg = (string)jsonObject["reisi_lopp_aeg"];
+                    var kiirus = (string)jsonObject["kiirus"];
+                    var latitude = (string)jsonObject["latitude"];
+                    var longitude = (string)jsonObject["longitude"];
+                    var rongi_suund = (string)jsonObject["rongi_suund"];
+                    var erinevus_plaanist = (string)jsonObject["erinevus_plaanist"];
+                    var lisateade = (string)jsonObject["lisateade"];
+                    var pohjus_teade = (string)jsonObject["pohjus_teade"];
+                    var avalda_kodulehel = (string)jsonObject["avalda_kodulehel"];
+                    var asukoha_uuendus = (string)jsonObject["asukoha_uuendus"];
+                    var reisi_staatus = (string)jsonObject["reisi_staatus"];
+                    var viimane_peatus = (string)jsonObject["viimane_peatus"];
+
+                    servElron.InsertServElron(
+                        reis: reis,
+                        liin: liin,
+                        reisiAlgusAeg: reisi_algus_aeg,
+                        reisiLoppAeg: reisi_lopp_aeg,
+                        kiirus: kiirus,
+                        latitude: latitude,
+                        longitude: longitude,
+                        rongiSuund: rongi_suund,
+                        erinevusPlaanist: erinevus_plaanist,
+                        lisateade: lisateade,
+                        pohjusTeade: pohjus_teade,
+                        avaldaKodulehel: avalda_kodulehel,
+                        asukohaUuendus: asukoha_uuendus,
+                        reisiStaatus: reisi_staatus,
+                        viimanePeatus: viimane_peatus
+                    );
+
+                }
+                
+            }
         }
     }
     
---
+++ gRPC_Service/ConsoleApp1/ServElron.cs
@@ -0,0 +1,57 @@
+namespace ConsoleApp1;
+
+using Npgsql;
+using System;
+
+public class ServElron
+{
+    private readonly string _connectionString;
+
+    public ServElron(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void InsertServElron(string reis, string liin, string reisiAlgusAeg, string reisiLoppAeg,
+                                string kiirus, string latitude, string longitude, string rongiSuund,
+                                string erinevusPlaanist, string lisateade, string pohjusTeade,
+                                string avaldaKodulehel, string asukohaUuendus, string reisiStaatus,
+                                string viimanePeatus)
+    {
+        int kiirusInt = int.Parse(kiirus);
+        decimal latitudeDecimal = decimal.Parse(latitude);
+        decimal longitudeDecimal = decimal.Parse(longitude);
+        int rongiSuundInt = int.Parse(rongiSuund);
+        DateTime asukohaUuendusDateTime = DateTime.Parse(asukohaUuendus);
+        DateTime asukohaUuendusUtc = asukohaUuendusDateTime.ToUniversalTime();
+
+        
+        using var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+
+        using var command = new NpgsqlCommand("INSERT INTO serv_elron (reis, liin, reisi_algus_aeg, " +
+            "reisi_lopp_aeg, kiirus, latitude, longitude, rongi_suund, erinevus_plaanist, lisateade, " +
+            "pohjus_teade, avalda_kodulehel, asukoha_uuendus, reisi_staatus, viimane_peatus) " +
+            "VALUES (@reis, @liin, @reisiAlgusAeg, @reisiLoppAeg, @kiirus, @latitude, @longitude, " +
+            "@rongiSuund, @erinevusPlaanist, @lisateade, @pohjusTeade, @avaldaKodulehel, " +
+            "@asukohaUuendus, @reisiStaatus, @viimanePeatus)", connection);
+
+        command.Parameters.AddWithValue("@reis", reis);
+        command.Parameters.AddWithValue("@liin", liin);
+        command.Parameters.AddWithValue("@reisiAlgusAeg", reisiAlgusAeg);
+        command.Parameters.AddWithValue("@reisiLoppAeg", reisiLoppAeg);
+        command.Parameters.AddWithValue("@kiirus", kiirus);
+        command.Parameters.AddWithValue("@latitude", latitude);
+        command.Parameters.AddWithValue("@longitude", longitude);
+        command.Parameters.AddWithValue("@rongiSuund", rongiSuund);
+        command.Parameters.AddWithValue("@erinevusPlaanist", erinevusPlaanist);
+        command.Parameters.AddWithValue("@lisateade", lisateade);
+        command.Parameters.AddWithValue("@pohjusTeade", pohjusTeade);
+        command.Parameters.AddWithValue("@avaldaKodulehel", avaldaKodulehel);
+        command.Parameters.AddWithValue("@asukohaUuendus", asukohaUuendusUtc);
+        command.Parameters.AddWithValue("@reisiStaatus", reisiStaatus);
+        command.Parameters.AddWithValue("@viimanePeatus", viimanePeatus);
+
+        command.ExecuteNonQuery();
+    }
+}
